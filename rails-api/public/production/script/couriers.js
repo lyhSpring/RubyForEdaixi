596145eb768751f25c25eeba1e70089236b9d0ab
wuyanli <278925389@qq.com>
2017-04-22 14:46:58 +0800

    添加取送人员选择站点

--- rails-api/public/production/script/couriers.js
+++ rails-api/public/production/script/couriers.js
@@ -18,6 +18,18 @@ function getCouriers(){
             var stringfortrlist = "";
             var stringforselect = "";
             if(data!=null){
+                var stringfortr ="<tr class=\"gradeX\">"+
+                "<td class=\"center\">"+id+"</td>"+
+                "<td >"+data[i].name+"</td>"+
+                "<td class=\"center hidden-xs\"><a href=\"#table-modal-showTaskSchedual\" data-toggle=\"modal\" class=\"btn btn-info\" onclick=\"showModel("+data.id+")\" style=\"font-size:4px;padding:0px 8px;\">"+"修改"+"</a></td>"+
+                   // "<td class=\"center hidden-xs\"><a class=\"btn btn-info\" style=\"font-size:4px;padding:0px 8px;\">删除</a></td>"+
+                  //  "<td class=\"center\" style=\"display:none\">"+data[i].id+"</td>"+
+		"<td >"+data[i].mobile+"</td>"+
+                "<td >"+data[i].email+"</td>"+   
+		 "</tr>";
+                    var strforname="<option>"+data[i].name+"</option>";
+                    stringforselect = stringforselect + strforname;
+                stringfortrlist = stringfortrlist + stringfortr; 
                 for(var i = 0; i < data.length; i++){
                     var stringfortr ="<tr class=\"gradeX\">"+
                     "<td class=\"center\">"+id+"</td>"+
@@ -38,7 +50,39 @@ function getCouriers(){
             $('#couriersTable').html(stringfortrlist);
         }
     });
-
 }
+
+function showModel(id){
+ $('#selectStopsModel').modal('show');
+    getStops();
+}
+
+function getStops(){
+//        $.ajax({
+//        type: "GET",
+//        url: domain+"/users/getUsersByRole?User[role]=qs",
+//        dataType: "json",
+//        success: function (data) {
+//            var stringforselect = "";
+//            if(data!=null){
+//                for(var i = 0; i < data.length; i++){
+//                    var strforname="<option>"+data[i].name+"</option>";
+//                    stringforselect = stringforselect + strforname;
+//                }
+//            }
+//
+//            $('#selectStops').html(stringforselect);
+//        }
+//    });
+//    
+    var stringforselect="";
+    for(var i=0;i<6;i++){
+        var strforselet="<option> 海淀"+i+"</option>";
+        stringforselect = stringforselect + strforselet;
+    }
+    $('#selectStops').html(stringforselect);
+    
+}
+
 
 
