b2d2d5f508a60cfafd3e9f3c4cf4ed1da4294b79
lyhSpring <347784324@qq.com>
2017-05-12 21:52:12 +0800

    Update product.rb

--- rails-api/app/models/product.rb
+++ rails-api/app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
+	has_one :price
 	def self.search(categories_id,page)
 		order('updated_at').where('categories_id = ?',"#{categories_id}").paginate(page: page, per_page: 10)
-			
-
 	end
 end
