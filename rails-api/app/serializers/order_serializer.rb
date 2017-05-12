69ea11a1806fd2747c05033003b14426623409a1
lyhSpring <347784324@qq.com>
2017-05-12 21:56:04 +0800

    Update order_serializer.rb

--- rails-api/app/serializers/order_serializer.rb
+++ rails-api/app/serializers/order_serializer.rb
@@ -1,3 +1,12 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :courier_id, :washing_status, :factory_id, :waybill_id, :voucher_status, :categories_id, :status, :address_id, :totalprice, :user_id
+  attributes :id, :courier_id, :washing_status, :factory_id, :waybill_id, :voucher_status, :categories_id, :status, :address_id, :totalprice, :user_id, :waybills, :items
+  def waybills
+  	# Waybill.where('order_id=?',object.id)
+  	object.waybills
+  end
+
+  def items
+  	# Item.where('order_id=?',object.id)
+  	object.items
+  end
 end
